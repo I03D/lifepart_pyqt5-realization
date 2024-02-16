c2c9dd7927fb9c9b1d0440535f869f8e19d7c97d
I03D <98839825+I03D@users.noreply.github.com>
2024-02-16 19:01:22 +0700

    Add files via upload

---
+++ lifepart_pyqt5-realization-main/blinker5.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QLabel, QMainWindow, QApplication
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore, QtWidgets
+import sys
+  
+class Window(QMainWindow): 
+    def __init__(self): 
+        super().__init__() 
+  
+        self.setWindowTitle("Label")
+
+        self.setGeometry(0, 0, size.width(), 100)
+        self.setStyleSheet("background-color: white;")
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint)
+
+        
+        pixmap = QPixmap('sun.png')
+        self.label = QLabel('test', self)
+        self.label.setGeometry(0, 0, size.width(), 100)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setPixmap(pixmap)
+        
+        timer = QTimer()
+        timer.singleShot(300, sys.exit)
+        self.show() 
+  
+App = QApplication(sys.argv) 
+screen = App.primaryScreen()
+size = screen.size()
+        
+window = Window() 
+  
+sys.exit(App.exec()) 
