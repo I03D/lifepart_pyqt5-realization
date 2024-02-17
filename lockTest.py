82cf74d11dcd71de90627fb3f6ff5459c0292190
I03D <98839825+I03D@users.noreply.github.com>
2024-02-17 17:28:38 +0700

    windows blink fix + windows lockstation check fix

--- lockTest.py
+++ lockTest.py
@@ -1,8 +1,8 @@
-import os, time
+import os, time, subprocess
 
 def test():
+    print('update test')
     if os.name == 'posix':
-        import subprocess
         process = 'i3lock'
 
         ps_output = subprocess.check_output(['ps', '-ef'])
@@ -10,25 +10,17 @@ def test():
 
         for line in ps_lines:
             if process in line:
-                # print('running!!')
                 return 1
                 break
         else:
-            # print('not running!')
             return 0
     elif os.name == 'nt':
-        import psutil
-        process = 'windows lock station (find right name)'
+        process = 'LogonUI.exe'
         pid = False
-
-        for proc in psutil.process_iter():
-            if process in proc.name().lower():
-                pid = True
-                break
-            if pid:
-                # print('found!!')
-                return 1
-            else:
-                # print('not found!')
-                return 0
-
+        
+        tasklist = str(subprocess.check_output('TASKLIST'))
+        if process in tasklist:
+            return 1
+        else:
+            return 0
+            
